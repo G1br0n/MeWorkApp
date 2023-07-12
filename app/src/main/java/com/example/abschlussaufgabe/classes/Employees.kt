3221b3f694252697fe932ca329135ed15ebc9486
G1br0n <127736981+G1br0n@users.noreply.github.com>
2023-07-12 13:55:03 +0200

    Log-In Fragment + IDs + Clean + ic_lanuncher Home Fragment + cv User + RV qualification + iv (noch nicht vertig)

---
+++ app/src/main/java/com/example/abschlussaufgabe/classes/Employees.kt
@@ -0,0 +1,15 @@
+package com.example.abschlussaufgabe.classes
+
+class Employees (
+    private val surName: String,  //Ring
+    private val firstName: String,  //Andrej
+    private val birthData: String,  //12.08.1989
+    private val baNumber: String, //9837454
+    private val bupNumber: String, //4092834
+
+    private val workingPosition: MutableMap<String,String>,
+
+
+
+        )
+
--- app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
+++ app/src/main/java/com/example/abschlussaufgabe/ui/HomeFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [logInFragment.newInstance] factory method to
+ * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class logInFragment : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class logInFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_log_in, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class logInFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment logInFragment.
+         * @return A new instance of fragment HomeFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            logInFragment().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
